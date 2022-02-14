dd9772eb23dfa4f29133e20fecec9e49a38102bc
annyCode <ig.joseanny@gmail.com>
2022-02-13 23:46:09 -0300

    order report

---
+++ src/main/java/com/letscode/model/SortByAge.java
@@ -0,0 +1,14 @@
+package com.letscode.model;
+
+import java.util.Comparator;
+
+public class SortByNumber implements Comparator<Rebel> {
+
+    @Override
+    public int compare(Rebel r1, Rebel r2) {
+        if(r1.getAge() < r2.getAge()){
+            return 1;
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/com/letscode/model/SortByName.java
@@ -0,0 +1,2 @@
+package com.letscode.model;public class sortByName {
+}
